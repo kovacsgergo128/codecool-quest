e4e721944d78f1e3ac90f8ff06a310446ca24fe0
sandormatyas <sandormatyas7@gmail.com>
2020-01-14 13:09:39 +0100

    change move condition to use isOccupied()

--- src/main/java/com/codecool/quest/logic/actors/Actor.java
+++ src/main/java/com/codecool/quest/logic/actors/Actor.java
@@ -15,7 +15,7 @@ public abstract class Actor implements Drawable {
 
     public void move(int dx, int dy) {
         Cell nextCell = cell.getNeighbor(dx, dy);
-        if (!nextCell.getType().equals(CellType.WALL)) {
+        if (!nextCell.isOccupied()) {
             cell.setActor(null);
             nextCell.setActor(this);
             cell = nextCell;
