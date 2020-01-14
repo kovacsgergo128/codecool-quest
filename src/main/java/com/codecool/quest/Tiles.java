893ed89858e2dde5e475377f552c2bae5e99b14a
sandormatyas <sandormatyas7@gmail.com>
2020-01-14 14:44:49 +0100

    add new tiles openDoor and lockedDoor

--- src/main/java/com/codecool/quest/Tiles.java
+++ src/main/java/com/codecool/quest/Tiles.java
@@ -29,6 +29,8 @@ public class Tiles {
         tileMap.put("floor", new Tile(2, 0));
         tileMap.put("player", new Tile(27, 0));
         tileMap.put("skeleton", new Tile(29, 6));
+        tileMap.put("lockedDoor", new Tile(0, 9));
+        tileMap.put("openDoor", new Tile(2, 9));
     }
 
     public static void drawTile(GraphicsContext context, Drawable d, int x, int y) {
