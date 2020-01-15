492191e4b93ce69abf1914d9dfe0ed2a33a100bf
sandormatyas <sandormatyas7@gmail.com>
2020-01-15 12:48:47 +0100

    hotfix NullPointerException in test

--- src/main/java/com/codecool/quest/logic/actors/Player.java
+++ src/main/java/com/codecool/quest/logic/actors/Player.java
@@ -29,10 +29,10 @@ public class Player extends Actor {
     @Override
     public void move(int dx, int dy) {
         Cell nextCell = cell.getNeighbor(dx, dy);
-        if (nextCell.getDoor() != null && nextCell.getDoor().isLocked() && this.inventory.contains("key")) {
+        if (nextCell != null && nextCell.getDoor() != null && nextCell.getDoor().isLocked() && this.inventory.contains("key")) {
             this.inventory.removeItemByItemName("key");
             nextCell.getDoor().openDoor();
-        } else if (nextCell.getDecor() != null && (nextCell.getDecor().getTileName().equals("bonfire") || nextCell.getDecor().getTileName().equals("spikes"))) {
+        } else if (nextCell != null && nextCell.getDecor() != null && (nextCell.getDecor().getTileName().equals("bonfire") || nextCell.getDecor().getTileName().equals("spikes"))) {
             this.setHealth(this.getHealth() - 2);
         }
         super.move(dx, dy);
